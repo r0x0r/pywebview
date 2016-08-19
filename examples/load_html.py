a46c7579aa2713b5182c44e1e77c5130ff2fba24
Roman Sirokov <roman@flowrl.com>
2016-08-20 00:13:12 +0300

    Add load html example

---
+++ examples/load_html.py
@@ -0,0 +1,20 @@
+import webview
+import threading
+
+"""
+This example demonstrates how to load HTML in a web view window
+"""
+
+def change_url():
+    import time
+    time.sleep(5)
+    webview.load_html("<h1>This is dynamically loaded HTML</h1>")
+
+
+if __name__ == '__main__':
+    t = threading.Thread(target=change_url)
+    t.start()
+
+    # Create a non-resizable webview window with 800x600 dimensions
+    webview.create_window("Simple browser", width=800, height=600, resizable=True)
+
