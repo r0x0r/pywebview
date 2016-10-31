4024a7acfb7b10e92b6f243c1aa4483a36391fcf
Roman <roman@flowrl.com>
2016-10-31 23:11:41 +0200

    Simplify simple_browser example

--- examples/simple_browser.py
+++ examples/simple_browser.py
@@ -5,6 +5,6 @@ This example demonstrates how to create a webview window.
 """
 
 if __name__ == '__main__':
-    # Create a non-resizable webview window with 800x600 dimensions
-    webview.create_window("Simple browser", "http://www.flowrl.com", width=800, height=600, resizable=False)
+    # Create a standard webview window
+    webview.create_window("Simple browser", "http://www.flowrl.com")
 
