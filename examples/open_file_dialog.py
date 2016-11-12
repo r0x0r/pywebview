dcc472a6c8e15e7fc105277332681b38e40640df
Roman Sirokov <roman@flowrl.com>
2016-11-13 01:49:17 +0200

    Revert open_file_dialog example

--- examples/open_file_dialog.py
+++ examples/open_file_dialog.py
@@ -8,7 +8,7 @@ This example demonstrates creating an open file dialog.
 def open_file_dialog():
     import time
     time.sleep(5)
-    print(webview.create_file_dialog(webview.OPEN_DIALOG, allow_multiple=False))
+    print(webview.create_file_dialog(webview.OPEN_DIALOG, allow_multiple=True))
 
 
 if __name__ == '__main__':
@@ -16,4 +16,5 @@ if __name__ == '__main__':
     t.start()
 
     webview.create_window("Open file dialog example", "http://www.flowrl.com")
+
 
