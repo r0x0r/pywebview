ff49136dfa1324d4e5e20b979603eeda9972de49
Roman <roman@flowrl.com>
2017-02-09 00:33:34 +0200

    Update flask example with toggle_fullscreen and link opening

--- examples/toggle_fullscreen.py
+++ examples/toggle_fullscreen.py
@@ -5,10 +5,6 @@ import threading
 This example demonstrates how to toggle fullscreen mode programmatically.
 """
 
-<<<<<<< b1bd30429e64d4a865326a8d9142e22eb5e9e501
-
-=======
->>>>>>> Implement toggle_fullscreen functionality
 def toggle_fullscreen():
     import time
     time.sleep(5)
@@ -20,4 +16,3 @@ if __name__ == '__main__':
     t.start()
 
     webview.create_window("Full-screen window", "http://www.flowrl.com")
-
