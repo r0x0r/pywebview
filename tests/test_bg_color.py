41eea121dff7d5f83c55b99f3e915f2dc948185a
Roman <antacid@gmail.com>
2017-05-16 09:05:37 +0300

    Update test_bg_color.py

--- tests/test_bg_color.py
+++ tests/test_bg_color.py
@@ -5,7 +5,7 @@ from .util import destroy_window, run_test
 def bg_color():
     import webview
     destroy_window(webview)
-    webview.create_window('Background color test', 'https://www.example.org', bg_color='#0000FF')
+    webview.create_window('Background color test', 'https://www.example.org', background_color='#0000FF')
 
 
 def test_bg_color():
