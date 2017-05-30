57991e6232a0f7fe081aba03ce2beb493ff110ff
Roman <roman@flowrl.com>
2017-05-31 00:03:59 +0300

    Add invalid bg_color test cases

--- tests/test_bg_color.py
+++ tests/test_bg_color.py
@@ -1,5 +1,4 @@
 import pytest
-import webview
 from .util import destroy_window, run_test
 
 def bg_color():
@@ -8,5 +7,28 @@ def bg_color():
     webview.create_window('Background color test', 'https://www.example.org', background_color='#0000FF')
 
 
+def invalid_bg_color():
+    import webview
+
+    with pytest.raises(ValueError):
+        webview.create_window('Background color test', 'https://www.example.org', background_color='#dsg0000FF')
+
+    with pytest.raises(ValueError):
+        webview.create_window('Background color test', 'https://www.example.org', background_color='FF00FF')
+
+    with pytest.raises(ValueError):
+        webview.create_window('Background color test', 'https://www.example.org', background_color='#ac')
+
+    with pytest.raises(ValueError):
+        webview.create_window('Background color test', 'https://www.example.org', background_color='#EFEFEH')
+
+    with pytest.raises(ValueError):
+        webview.create_window('Background color test', 'https://www.example.org', background_color='#0000000')
+
+
 def test_bg_color():
     run_test(bg_color)
+
+
+def test_invalid_bg_color():
+    run_test(invalid_bg_color)
