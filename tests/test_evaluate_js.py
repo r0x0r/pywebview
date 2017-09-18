f7e9de1c6cbf5c903c4547212ad89a17b317d35c
Roman <roman@flowrl.com>
2017-09-19 00:33:23 +0300

    Add evaluate_js test

---
+++ tests/test_evaluate_js.py
@@ -0,0 +1,28 @@
+import pytest
+import threading
+from .util import run_test, destroy_window
+
+
+def evaluate_js():
+    import webview
+
+    def _evaluate_js(webview):
+        result = webview.evaluate_js("""
+            document.body.style.backgroundColor = '#212121';
+            // comment
+            function test() {
+                return 2 + 2;
+            }
+            test();
+        """)
+        assert result == 4
+
+    t = threading.Thread(target=_evaluate_js, args=(webview,))
+    t.start()
+    destroy_window(webview)
+
+    webview.create_window('Evaluate JS test', 'https://www.example.org')
+
+
+def test_load_html():
+    run_test(evaluate_js)
