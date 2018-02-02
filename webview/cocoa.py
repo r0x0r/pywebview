4ccd2790bce000c697c338ab1960bf5041450874
Roman <roman@flowrl.com>
2018-02-03 01:32:12 +0200

    [Cocoa] Remove duplicate webview_ready.set

--- webview/cocoa.py
+++ webview/cocoa.py
@@ -573,7 +573,6 @@ def create_window(uid, title, url, width, height, resizable, fullscreen, min_siz
     def create():
         browser = BrowserView(uid, title, url, width, height, resizable, fullscreen, min_size,
                               confirm_quit, background_color, debug, js_api, webview_ready)
-        webview_ready.set()
         browser.show()
         webview_ready.set()
 
