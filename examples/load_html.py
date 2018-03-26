4f32fa58b8d66ce576239aef87971f4c03a37464
Roman Sirokov <roman.sirokov@siili.com>
2018-03-26 23:38:29 +0300

    [WinForms] Add base_uri support

--- examples/load_html.py
+++ examples/load_html.py
@@ -9,6 +9,7 @@ This example demonstrates how to load HTML in a web view window
 def load_html():
     webview.load_html('<h1>This is dynamically loaded HTML</h1>')
 
+
 if __name__ == '__main__':
     t = threading.Thread(target=load_html)
     t.start()
