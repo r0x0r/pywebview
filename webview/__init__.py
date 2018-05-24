1b54fdeae648cdb54748b71116db95577d992ba4
Roman Sirokov <roman@flowrl.com>
2018-05-24 21:42:52 +0300

    Change window creation logic to allow subsequent master window openings

--- webview/__init__.py
+++ webview/__init__.py
@@ -151,7 +151,7 @@ def create_window(title, url=None, js_api=None, width=800, height=600,
     :param title: Window title
     :param url: URL to load
     :param width: window width. Default is 800px
-    :param height:window height. Default is 600px
+    :param height:window h  eight. Default is 600px
     :param resizable True if window can be resized, False otherwise. Default is True
     :param fullscreen: True if start in fullscreen mode. Default is False
     :param min_size: a (width, height) tuple that specifies a minimum window size. Default is 200x100
@@ -161,21 +161,23 @@ def create_window(title, url=None, js_api=None, width=800, height=600,
     :param text_select: Allow text selection on page. Default is False.
     :return: The uid of the created window.
     """
-    uid = 'child_' + uuid4().hex[:8]
 
     valid_color = r'^#(?:[0-9a-fA-F]{3}){1,2}$'
     if not re.match(valid_color, background_color):
         raise ValueError('{0} is not a valid hex triplet color'.format(background_color))
 
     if not _initialized:
-        # Check if starting up from main thread; if not, wait; finally raise exception
-        if current_thread().name != 'MainThread':
-            if not _webview_ready.wait(5):
-                raise Exception('Call create_window from the main thread first, and then from subthreads')
-        else:
-            _initialize_imports()
-            localization.update(strings)
-            uid = 'master'
+        _initialize_imports()
+        localization.update(strings)
+
+    # Check if starting up from main thread; if not, wait; finally raise exception
+    if current_thread().name == 'MainThread':
+        uid = 'master'
+    else:
+        uid = 'child_' + uuid4().hex[:8]
+
+        if not _webview_ready.wait(5):
+            raise Exception('Call create_window from the main thread first, and then from subthreads')
 
     _webview_ready.clear()  # Make API calls wait while the new window is created
     gui.create_window(uid, make_unicode(title), transform_url(url),
