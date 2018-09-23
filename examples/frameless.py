9cc138c99b9a7707a07f97b3874132a5687e3c4a
Roman <roman@flowrl.com>
2018-09-23 23:53:59 +0300

    [WinForms] Implement frameless window + easy drag

---
+++ examples/frameless.py
@@ -0,0 +1,12 @@
+import webview
+
+"""
+This example demonstrates how to create a frameless window with a custom minimum
+size.
+"""
+
+if __name__ == '__main__':
+    # Create a resizable webview window with minimum size constraints
+    webview.create_window("Frameless window",
+                          "http://pywebview.flowrl.com",
+                          frameless=True)
