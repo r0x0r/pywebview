79ddf20b4373f7bb04a475c57fc0ddb1f82d89fc
Roman Sirokov <antacid@gmail.com>
2019-06-21 12:15:09 +0300

    Add http server test

---
+++ tests/test_http_server.py
@@ -0,0 +1,32 @@
+import os
+from time import sleep
+from .util import run_test, assert_js
+import webview
+
+class Api:
+    def test(self, params):
+        return 'JS Api is working too'
+
+
+def test_start():
+    api = Api()
+    window = webview.create_window('Relative URL test', 'assets/test.html', js_api=api)
+    run_test(webview, window, assert_func, start_args={'http_server': True})
+
+
+def assert_func(window):
+    sleep(1)
+    html_result = window.evaluate_js('document.getElementById("heading").innerText')
+    assert html_result == 'Hello there!'
+
+    css_result = window.evaluate_js('window.getComputedStyle(document.body, null).getPropertyValue("background-color")')
+    assert css_result == 'rgb(255, 0, 0)'
+
+    js_result = window.evaluate_js('window.testResult')
+    assert js_result == 80085
+
+    assert_js(window, 'test', 'JS Api is working too')
+
+
+
+
