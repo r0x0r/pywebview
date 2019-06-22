34f35f4a803b27a214497350b13ad5740f168e6e
Roman Sirokov <antacid@gmail.com>
2019-06-22 14:06:18 +0300

    Add video example

--- examples/http_server.py
+++ examples/http_server.py
@@ -5,5 +5,5 @@ import webview
 This example demonstrates how to use a built-in http server to serve local files
 """
 if __name__ == '__main__':
-    webview.create_window('My first HTML5 application', 'index.html')
+    webview.create_window('My first HTML5 application', 'assets/index.html')
     webview.start(http_server=True)
