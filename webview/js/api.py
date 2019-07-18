81dabd09008d58e0589536701a9adabf2975ceac
Konstantin Vasserman <32774057+kvasserman@users.noreply.github.com>
2019-07-18 04:39:40 -0400

    Don't escape line breaks in result of js_bridge_call (#349)

--- webview/js/api.py
+++ webview/js/api.py
@@ -46,7 +46,7 @@ window.pywebview = {
             if (returnObj.isSet) {
                 returnObj.isSet = false;
                 try {
-                    resolve(JSON.parse(returnObj.value.replace(/\\n/, '\\\\n')));
+                    resolve(JSON.parse(returnObj.value));
                 } catch(e) {
                     resolve(returnObj.value);
                 }
