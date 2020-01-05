fe2bb213a1b1ac7dddb66f96e3c6ce665660dec7
Roman <roman@flowrl.com>
2020-01-05 09:51:50 +0200

    Fix CEF settings

--- examples/cef.py
+++ examples/cef.py
@@ -1,13 +1,14 @@
 import webview
-from webview.platforms.cef import settings
 
 """
 This example demonstrates how to create a CEF window. Available only on Windows.
 """
 
-settings = {
+# To pass custom settings to CEF, import and update settings dict
+from webview.platforms.cef import settings
+settings.update({
     'persist_session_cookies': True
-}
+})
 
 if __name__ == '__main__':
     webview.create_window('CEF browser', 'https://pywebview.flowrl.com/hello')
