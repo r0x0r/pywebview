67364b365372a9e3d0fc231bb24269e86054aa68
zvibazak <zvibazak@gmail.com>
2020-02-12 10:16:30 +0200

    Update window.py

--- webview/window.py
+++ webview/window.py
@@ -299,8 +299,11 @@ class Window:
         for func in functions:
             name = func.__name__
             self._functions[name] = func
-
-            params = list(inspect.getargspec(func).args)
+            try:
+                params = list(inspect.getfullargspec(func).args) # Python 3
+            except AttributeError:
+                params = list(inspect.getargspec(func).args)  # Python 2
+            
 
             func_list.append({
                 'func': name,
