676c037255e9185f18c6ed9f63044df3ffe1b16f
Roman Sirokov <antacid@gmail.com>
2020-04-04 00:40:14 +0300

    [Cocoa] implement on_top

--- webview/window.py
+++ webview/window.py
@@ -310,12 +310,12 @@ class Window:
         for func in functions:
             name = func.__name__
             self._functions[name] = func
-            
+
             try:
                 params = list(inspect.getfullargspec(func).args) # Python 3
             except AttributeError:
                 params = list(inspect.getargspec(func).args)  # Python 2
-            
+
 
             func_list.append({
                 'func': name,
