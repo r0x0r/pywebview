3754213b3c6d5546a9a2c9d7469f5d467db7da81
Roman Sirokov <antacid@gmail.com>
2020-06-30 13:21:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,12 +53,11 @@ setup(
         'Environment :: X11 Applications :: GTK',
         'Environment :: X11 Applications :: Qt',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: User Interfaces'
