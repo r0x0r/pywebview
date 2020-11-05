3eaf2f90e5f9cc08daa1278010d02cacf46a49b4
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-11-05 08:45:42 +0000

    multiprocessing
    example added

---
+++ examples/noblock.py
@@ -0,0 +1,11 @@
+import webview
+
+"""
+This example demonstrates how to stop freezing main thread.
+"""
+
+if __name__ == '__main__':
+    # Create a standard webview window
+    window = webview.create_window('Simple browser', 'https://pywebview.flowrl.com/hello')
+    webview.start(block=False)
+    print('free main thread')
