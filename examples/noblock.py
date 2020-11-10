f3309b95614b051b5c3c87e9b259ea1821010107
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-11-10 15:59:55 +0000

    Update noblock.py

--- examples/noblock.py
+++ examples/noblock.py
@@ -6,6 +6,9 @@ This example demonstrates how to stop freezing main thread.
 
 if __name__ == '__main__':
     # Create a standard webview window
-    window = webview.create_window('Simple browser', 'https://pywebview.flowrl.com/hello')
-    webview.start(block=False)
+    window = webview.create_window('Tab1', 'https://pywebview.flowrl.com/')
+    process = webview.start(block=False)
     print('free main thread')
+    window2 = webview.create_window('Tab2', 'https://pywebview.flowrl.com/hello')
+    process.join()
+    # blocked main thread
