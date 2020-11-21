a3817c7964dd9c27b122f543402914ce64acdc5b
sbbosco <73964492+sbbosco@users.noreply.github.com>
2020-11-20 18:17:46 -0700

    Cleanup load_html http code

--- webview/platforms/edgechromium.py
+++ webview/platforms/edgechromium.py
@@ -63,20 +63,18 @@ class EdgeChrome:
         self.web_view.NavigationCompleted += self.on_navigation_completed
         self.web_view.WebMessageReceived += self.on_script_notify
 
-        self.httpd = None # HTTP server for load_html
-        self.tmpdir = None
         self.url = None
         self.ishtml = False
-
-        if window.html or 'localhost' in window.real_url or '127.0.0.1' in window.real_url:
-            _allow_localhost()
+        self.html = None
 
         if window.real_url:
             self.load_url(window.real_url)
         elif window.html:
-            self.load_html(window.html, '')
+            self.html = window.html
+            self.web_view.EnsureCoreWebView2Async(None)
         else:
-            self.load_html(default_html, '')
+            self.html = default_html
+            self.web_view.EnsureCoreWebView2Async(None)
 
     def evaluate_js(self, script, callback=None):
         def _callback(result):
@@ -104,19 +102,9 @@ class EdgeChrome:
     def get_current_url(self):
         return self.url
 
-    def load_html(self, html, base_uri):
-        self.tmpdir = tempfile.mkdtemp()
-        self.temp_html = os.path.join(self.tmpdir, 'index.html')
-
-        with open(self.temp_html, 'w', encoding='utf-8') as f:
-            f.write(inject_base_uri(html, base_uri))
-
-        if self.httpd:
-            self.httpd.shutdown()
-
-        url = resolve_url('file://' + self.temp_html, True)
+    def load_html(self, sender, html):
         self.ishtml = True
-        self.web_view.Source = Uri(url)
+        sender.CoreWebView2.NavigateToString(html)
 
     def load_url(self, url):
         self.ishtml = False
@@ -142,26 +130,20 @@ class EdgeChrome:
     def on_webview_ready(self, sender, args):
         sender.CoreWebView2.NewWindowRequested += self.on_new_window_request
         settings = sender.CoreWebView2.Settings
-        if not _debug: settings.AreDefaultContextMenusEnabled = False
+        settings.AreDefaultContextMenusEnabled = _debug 
         settings.AreDefaultScriptDialogsEnabled = True
-        settings.AreDevToolsEnabled = True
+        settings.AreDevToolsEnabled = _debug
         settings.IsBuiltInErrorPageEnabled = True 
         settings.IsScriptEnabled = True
         settings.IsWebMessageEnabled = True
-        settings.IsStatusBarEnabled = True
+        settings.IsStatusBarEnabled = _debug
         settings.IsZoomControlEnabled = True
+        if self.html: self.load_html(sender, self.html)
         
     def on_navigation_start(self, sender, args):
         pass
 
     def on_navigation_completed(self, sender, args):
-        try:
-            if self.tmpdir and os.path.exists(self.tmpdir):
-                shutil.rmtree(self.tmpdir)
-                self.tmpdir = None
-        except Exception as e:
-            logger.exception('Failed deleting %s' % self.tmpdir)
-
         url = str(sender.Source)
         self.url = None if self.ishtml else url
         self.web_view.ExecuteScriptAsync('window.alert = (msg) => window.chrome.webview.postMessage(["alert", msg+"", ""])')
@@ -175,30 +157,3 @@ class EdgeChrome:
             self.web_view.ExecuteScriptAsync(disable_text_select)
 
         self.pywebview_window.loaded.set()
-
-def _allow_localhost():
-    import subprocess
-
-    # lifted from https://github.com/pyinstaller/pyinstaller/wiki/Recipe-subprocess
-    def subprocess_args(include_stdout=True):
-        if hasattr(subprocess, 'STARTUPINFO'):
-            si = subprocess.STARTUPINFO()
-            si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-            env = os.environ
-        else:
-            si = None
-            env = None
-
-        if include_stdout:
-            ret = {'stdout': subprocess.PIPE}
-        else:
-            ret = {}
-
-        ret.update({'stdin': subprocess.PIPE,
-                    'stderr': subprocess.PIPE,
-                    'startupinfo': si,
-                    'env': env })
-        return ret
-
-    output = subprocess.check_output('checknetisolation LoopbackExempt -s', **subprocess_args(False))
-        
