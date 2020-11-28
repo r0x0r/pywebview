2413ac86c431f813ba672f0dbf006265ea7bdc1d
Nuno <ulukyn@gmail.com>
2020-11-28 01:04:07 +0100

    Qt window icon don't need check nor extension

--- webview/platforms/qt.py
+++ webview/platforms/qt.py
@@ -231,9 +231,7 @@ class BrowserView(QMainWindow):
         self.title = window.title
         self.setWindowTitle(window.title)
 
-        self.icon = window.icon+".png"
-        if os.path.isfile(window.icon):
-            self.setWindowIcon(QIcon(window.icon))
+        self.setWindowIcon(QIcon(window.icon))
 
         # Set window background color
         self.background_color = QColor()
