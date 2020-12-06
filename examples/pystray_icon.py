1f8845f89aa936379fbea4d8707fbb4887c62696
Alex Covizzi <alexcovizzi@gmail.com>
2020-12-06 11:14:19 +0100

    Fix process spawn on Mac os, simplify logic

--- examples/pystray_icon.py
+++ examples/pystray_icon.py
@@ -5,7 +5,9 @@ import webview
 import sys
 if sys.platform == 'darwin':
     # System tray icon needs to run in it's own process on Mac OS X
+    import multiprocessing
     from multiprocessing import Process as Thread, Queue
+    multiprocessing.set_start_method('spawn')
 else:
     from threading import Thread
     from queue import Queue
@@ -19,7 +21,6 @@ This example demonstrates running pywebview alongside with pystray to display a 
 def run_webview():
     window = webview.create_window('Webview', 'https://pywebview.flowrl.com/hello')
     webview.start()
-    return window
 
 
 def run_pystray(queue: Queue):
@@ -43,16 +44,13 @@ if __name__ == '__main__':
     icon_thread = Thread(target=run_pystray, args=(queue,))
     icon_thread.start()
 
-    window = run_webview()
+    run_webview()
 
     while True:
         event = queue.get()
         if event == 'open':
-            if window.closed.is_set():
-                window = run_webview()
+            run_webview()
         if event == 'exit':
-            if not window.closed.is_set():
-                window.destroy()
             break
 
     icon_thread.join()
