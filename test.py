51647bff59b8ae73dc8d777c88544d683e80d7b3
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-12-24 05:38:53 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+import os
+from time import sleep
+from tests.util import run_test, assert_js
+import webview
+import pyscreenshot as ImageGrab
+
+
+def test_start():
+    api = Api()
+    window = webview.create_window('Relative URL test', 'assets/test.html', js_api=api)
+    run_test(webview, window, assert_func, start_args={'block' : False})
+    print('noblock/multiprocessing test is working.')
+
+
+def assert_func(window):
+    sleep(1)
+    # grab fullscreen
+    im = ImageGrab.grab()
+
+    # save image file
+    im.save('alert.png')
