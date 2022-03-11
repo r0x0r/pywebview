d8b350b4493e251ff73d242e46d276090bf71cf7
Roman Sirokov <roman@flowrl.com>
2022-03-11 09:21:25 +0200

    Clean up code

--- examples/todos/start.py
+++ examples/todos/start.py
@@ -26,5 +26,5 @@ class Api():
 if __name__ == '__main__':
     api = Api()
     webview.create_window('Todos magnificos', 'assets/index.html', js_api=api, min_size=(600, 450))
-    webview.start(http_server=True)
+    webview.start()
 
