2708a688da2a42a81d74ac1913b37d0b5ee1ab37
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2022-04-23 14:09:00 -0400

    add app menu example

---
+++ examples/app_menu.py
@@ -0,0 +1,49 @@
+import webview
+import webview.menu as wm
+
+"""
+This example demonstrates how to create an application menu
+"""
+
+
+def change_active_window_content():
+    active_window = webview.active_window()
+    if active_window:
+        active_window.load_html('<h1>You changed this window!</h1>')
+
+def click_me():
+    active_window = webview.active_window()
+    if active_window:
+        active_window.load_html('<h1>You clicked me!</h1>')
+
+def do_nothing():
+    pass
+
+
+if __name__ == '__main__':
+    window_1 = webview.create_window('Application Menu Example', 'https://pywebview.flowrl.com/hello')
+    window_2 = webview.create_window('Another Window', html='<h1>Another window to test application menu</h1>')
+
+    menu_items = [
+        wm.Menu(
+            'Test Menu',
+            [
+                wm.MenuAction('Change Active Window Content', change_active_window_content),
+                wm.MenuSeparator(),
+                wm.Menu(
+                    'Random',
+                    [
+                        wm.MenuAction('Click Me', click_me)
+                    ]
+                )
+            ]
+        ),
+        wm.Menu(
+            'Nothing Here',
+            [
+                wm.MenuAction('This will do nothing', do_nothing)
+            ]
+        )
+    ]
+
+    webview.start(app_menu_list=menu_items)
