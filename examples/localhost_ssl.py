54d5cc64e79493556deeedb117d5f17665412655
Derek Anderson <public@kered.org>
2023-02-14 10:46:11 -0800

    change gui back to default

--- examples/localhost_ssl.py
+++ examples/localhost_ssl.py
@@ -2,6 +2,6 @@ import webview
 
 if __name__ == '__main__':
   webview.create_window('Local SSL Test', 'assets/index.html')
-  gui = 'qt' # or 'gtk'
+  gui = None
   webview.start(gui=gui, ssl=True)
 
