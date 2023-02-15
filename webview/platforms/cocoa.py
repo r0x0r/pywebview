r0x0r/pywebview

commit b40511d7b58d9ad4be76ba4745b54dc13ae11a95

[GTK / QT] Private mode fixes

webview/platforms/cocoa.py
@@ -8,7 +8,7 @@ import json
 import logging
 import webbrowser
 import ctypes
-from threading import Event, Semaphore
+from threading import Semaphore
 
 import Foundation
 import AppKit
@@ -16,7 +16,7 @@ import WebKit
 from PyObjCTools import AppHelper
 from objc import _objc, nil, super, registerMetaDataForSelector
 
-from webview import _debug, _user_agent, _incognito, OPEN_DIALOG, FOLDER_DIALOG, SAVE_DIALOG, parse_file_type, windows
+from webview import _debug, _user_agent, _private_mode, OPEN_DIALOG, FOLDER_DIALOG, SAVE_DIALOG, parse_file_type, windows
 from webview.util import parse_api_js, default_html, js_bridge_call
 from webview.js.css import disable_text_select
 from webview.screen import Screen
@@ -380,28 +380,23 @@ class BrowserView:
         config = self.webkit.configuration()
         config.userContentController().addScriptMessageHandler_name_(self._browserDelegate, 'browserDelegate')
 
-        if _incognito:
-            datastore = WebKit.WKWebsiteDataStore.nonPersistentDataStore()
+        if _private_mode:
+            # nonPersisentDataStore preserves cookies for some unknown reason. For this reason we use default datastore
+            # and clear all the cookies beforehand
+            datastore = WebKit.WKWebsiteDataStore.defaultDataStore()
 
-            def show_cookies(cookies):
-                print(cookies)
+            def dummy_completion_handler():
+                pass
 
-            # for datatype in WebKit.WKWebsiteDataStore.allWebsiteDataTypes():
-            #     datastore.removeDataOfTypes_forDataRecords_completionHandler_(datatype)
+            from_start = WebKit.NSDate.dateWithTimeIntervalSince1970_(0)
             config.setWebsiteDataStore_(datastore)
-            config.websiteDataStore().httpCookieStore().getAllCookies_(show_cookies)
+            datastore.removeDataOfTypes_modifiedSince_completionHandler_(['WKWebsiteDataTypeCookies'], from_start, dummy_completion_handler)
         else:
-            config.setWebsiteDataStore_(WebKit.WKWebsiteDataStore.defaultDataStore())
+            datastore = WebKit.WKWebsiteDataStore.defaultDataStore()
+            config.setWebsiteDataStore_(datastore)
 
-        try:
-            config.preferences().setValue_forKey_(Foundation.NO, 'backspaceKeyNavigationEnabled')
-        except:
-            pass
-
-        try:
-            config.preferences().setValue_forKey_(True, 'allowFileAccessFromFileURLs')
-        except:
-            pass
+        config.preferences().setValue_forKey_(Foundation.NO, 'backspaceKeyNavigationEnabled')
+        config.preferences().setValue_forKey_(True, 'allowFileAccessFromFileURLs')
 
         if _debug['mode']:
             config.preferences().setValue_forKey_(Foundation.YES, 'developerExtrasEnabled')

webview/__init__.py
@@ -13,7 +13,6 @@ http://github.com/r0x0r/pywebview/
 
 
 import logging
-import sys
 import os
 import re
 import threading
@@ -22,7 +21,6 @@ from proxy_tools import module_property
 
 import webview.http as http
 
-from webview.event import Event
 from webview.guilib import initialize
 from webview.util import _token, base_uri, parse_file_type, is_local_url, escape_string, escape_line_breaks, WebViewException
 from webview.window import Window
@@ -55,7 +53,6 @@ FOLDER_DIALOG = 20
 SAVE_DIALOG = 30
 
 DRAG_REGION_SELECTOR = '.pywebview-drag-region'
-DEFAULT_HTTP_PORT = 42001
 
 guilib = None
 _debug = {
@@ -63,12 +60,13 @@ _debug = {
 }
 _user_agent = None
 _http_server = False
-_incognito = True
+_private_mode = True
+_storage_path = None
 
 token = _token
 windows = []
 
-def start(func=None, args=None, localization={}, gui=None, debug=False, http_server=False, http_port=None, user_agent=None, incognito=True):
+def start(func=None, args=None, localization={}, gui=None, debug=False, http_server=False, http_port=None, user_agent=None, private_mode=True, storage_path=None):
     """
     Start a GUI loop and display previously created windows. This function must
     be called from a main thread.
@@ -86,10 +84,11 @@ def start(func=None, args=None, localization={}, gui=None, debug=False, http_ser
         window, a separate HTTP server is spawned. This option is ignored for
         non-local URLs.
     :param user_agent: Change user agent string. Not supported in EdgeHTML.
-    :param incognito: Enable incognito mode. Default is True. Non-incognito mode
-        is considered experimental and not supported on all platforms/renderers.
+    :param private_mode: Enable private mode. In private mode, cookies and local storage are not preserved.
+           Default is True.
+    :param storage_path: Custom location for cookies and other website data
     """
-    global guilib, _debug, _http_server, _user_agent, _incognito
+    global guilib, _debug, _http_server, _user_agent, _private_mode, _storage_path
 
     def _create_children(other_windows):
         if not windows[0].events.shown.wait(10):
@@ -105,7 +104,11 @@ def start(func=None, args=None, localization={}, gui=None, debug=False, http_ser
 
     _user_agent = user_agent
     _http_server = http_server
-    _incognito = incognito
+    _private_mode = private_mode
+    _storage_path = storage_path
+
+    if _storage_path and _private_mode and not os.path.exists(_storage_path):
+        os.makedirs(_storage_path)
 
     original_localization.update(localization)
 
@@ -134,8 +137,6 @@ def start(func=None, args=None, localization={}, gui=None, debug=False, http_ser
     guilib = initialize(gui)
 
     if http_server or has_local_urls or guilib.renderer == 'gtkwebkit2':
-        if not _incognito and not http_port:
-            http_port = DEFAULT_HTTP_PORT
         prefix, common_path = http.start_server(urls, http_port)
     else:
         prefix, common_path = None, None

webview/http.py
@@ -2,13 +2,14 @@ import bottle
 import json
 import logging
 import os
+import sys
 import threading
 import random
 import socket
 import uuid
 
-
 from .util import abspath, is_local_url
+
 
 logger = logging.getLogger(__name__)
 root_path='/'

webview/platforms/cef.py
@@ -7,7 +7,6 @@ import webbrowser
 
 from ctypes import windll
 from functools import wraps
-from uuid import uuid1
 from threading import Event
 from cefpython3 import cefpython as cef
 from copy import copy
@@ -17,7 +16,6 @@ from webview.js.css import disable_text_select
 from webview.js import dom
 from webview import _debug, _user_agent
 from webview.util import parse_api_js, default_html, js_bridge_call
-from webview.window import FixPoint
 
 
 sys.excepthook = cef.ExceptHook
@@ -35,10 +33,7 @@ command_line_switches = {}
 def _set_dpi_mode(enabled):
     """
     """
-    try:
-        import _winreg as winreg  # Python 2
-    except ImportError:
-        import winreg  # Python 3
+    import winreg  # Python 3
 
     try:
         dpi_support = winreg.OpenKey(winreg.HKEY_CURRENT_USER,

webview/platforms/edgechromium.py
@@ -14,7 +14,7 @@ import webbrowser
 from threading import Semaphore
 from platform import architecture
 
-from webview import _debug, _user_agent, _incognito
+from webview import _debug, _user_agent, _private_mode
 from webview.util import parse_api_js, interop_dll_path, default_html, js_bridge_call
 from webview.js import alert
 from webview.js.css import disable_text_select
@@ -144,7 +144,7 @@ class EdgeChrome:
         if _user_agent:
             settings.UserAgent = _user_agent
 
-        if _incognito:
+        if _private_mode:
             # cookies persist even if UserDataFolder is in memory. We have to delete cookies manually.
             sender.CoreWebView2.CookieManager.DeleteAllCookies()
 

webview/platforms/gtk.py
@@ -6,12 +6,13 @@ http://github.com/r0x0r/pywebview/
 """
 import logging
 import json
+import os
 import webbrowser
 
 from uuid import uuid1
 from threading import Semaphore
 
-from webview import _debug, _user_agent, OPEN_DIALOG, FOLDER_DIALOG, SAVE_DIALOG, parse_file_type, escape_string, windows
+from webview import _debug, _private_mode, _user_agent, _storage_path, OPEN_DIALOG, FOLDER_DIALOG, SAVE_DIALOG, parse_file_type, escape_string, windows
 from webview.util import parse_api_js, default_html, js_bridge_call
 from webview.js.css import disable_text_select
 from webview.screen import Screen
@@ -29,6 +30,7 @@ from gi.repository import Gtk as gtk
 from gi.repository import Gdk
 from gi.repository import GLib as glib
 from gi.repository import WebKit2 as webkit
+from gi.repository import Soup
 
 
 # version of WebKit2 older than 2.2 does not support returning a result of javascript, so we
@@ -113,16 +115,38 @@ class BrowserView:
         self.js_bridge = BrowserView.JSBridge(window)
         self.text_select = window.text_select
 
+        storage_path = _storage_path or os.path.join(os.path.expanduser('~'), '.pywebview')
+
+        if not os.path.exists(storage_path):
+            os.makedirs(storage_path)
+
+        if not _private_mode:
+            web_context = webkit.WebContext.get_default()
+            cookie_manager = web_context.get_cookie_manager()
+            cookie_manager.set_persistent_storage(
+                os.path.join(storage_path, 'cookies'),
+                webkit.CookiePersistentStorage.SQLITE
+            )
+            #self.webview = webkit.WebView.new_with_context(web_context)
+        #else:
         self.webview = webkit.WebView()
+
         self.webview.connect('notify::visible', self.on_webview_ready)
         self.webview.connect('load_changed', self.on_load_finish)
         self.webview.connect('decide-policy', self.on_navigation)
 
         http.js_callback = self.on_js_callback
 
+        webkit_settings = self.webview.get_settings().props
         user_agent = settings.get('user_agent') or _user_agent
         if user_agent:
-            self.webview.get_settings().props.user_agent = user_agent
+            webkit_settings.user_agent = user_agent
+
+        webkit_settings.enable_media_stream = True
+        webkit_settings.enable_mediasource = True
+        webkit_settings.enable_webaudio = True
+        webkit_settings.enable_webgl = True
+        webkit_settings.javascript_can_access_clipboard = True
 
         if window.frameless:
             self.window.set_decorated(False)
@@ -144,9 +168,13 @@ class BrowserView:
             self.webview.set_background_color(wvbg)
 
         if _debug['mode']:
-            self.webview.get_settings().props.enable_developer_extras = True
+            webkit_settings.enable_developer_extras = True
         else:
             self.webview.connect('context-menu', lambda a,b,c,d: True) # Disable context menu
+
+        if _private_mode:
+            webkit_settings.enable_html5_database = False
+            webkit_settings.enable_html5_local_storage = False
 
         self.webview.set_opacity(0.0)
         scrolled_window.add(self.webview)
@@ -435,7 +463,7 @@ class BrowserView:
                 })""" % {
                     'js_api_endpoint': http.js_api_endpoint,
                     'unique_id': unique_id,
-                    'script':  script
+                    'script': script
                 }
 
         self.loaded.wait()

webview/platforms/qt.py
@@ -15,7 +15,7 @@ from uuid import uuid1
 from copy import deepcopy
 from threading import Semaphore, Event
 
-from webview import _debug, _user_agent, _incognito, OPEN_DIALOG, FOLDER_DIALOG, SAVE_DIALOG, windows
+from webview import _debug, _user_agent, _private_mode, _storage_path, OPEN_DIALOG, FOLDER_DIALOG, SAVE_DIALOG, windows
 from webview.window import Window, FixPoint
 from webview.util import default_html, parse_api_js, js_bridge_call
 from webview.js.css import disable_text_select
@@ -166,8 +166,8 @@ class BrowserView(QMainWindow):
 
     class WebPage(QWebPage):
         def __init__(self, parent=None):
-            if is_webengine and _incognito:
-                super(BrowserView.WebPage, self).__init__(BrowserView.otr_profile, parent)
+            if is_webengine:
+                super(BrowserView.WebPage, self).__init__(BrowserView.profile, parent)
             else:
                 super(BrowserView.WebPage, self).__init__(parent)
             if is_webengine:
@@ -654,10 +654,15 @@ def create_window(window):
         global _app
         _app = QApplication.instance() or QApplication([])
 
-        if is_webengine and _incognito:
-            BrowserView.otr_profile = QWebEngineProfile()
-        elif not is_webengine and not _incognito:
-            logger.warning('qtwebkit does not support incognito=False')
+        if is_webengine:
+            if _private_mode:
+                BrowserView.profile = QWebEngineProfile()
+            else:
+                storage_path = _storage_path or os.path.join(os.path.expanduser('~'), '.pywebview')
+                BrowserView.profile = QWebEngineProfile('pywebview')
+                BrowserView.profile.setPersistentStoragePath(storage_path)
+        elif not is_webengine and not _private_mode:
+            logger.warning('qtwebkit does not support _private_mode=False')
 
         _create()
         _app.exec_()

webview/platforms/winforms.py
@@ -16,7 +16,7 @@ from uuid import uuid4
 from platform import machine
 import time
 
-from webview import windows, _incognito, OPEN_DIALOG, FOLDER_DIALOG, SAVE_DIALOG
+from webview import windows, _private_mode, _storage_path, OPEN_DIALOG, FOLDER_DIALOG, SAVE_DIALOG
 from webview.guilib import forced_gui_
 from webview.util import parse_file_type, inject_base_uri
 from webview.screen import Screen
@@ -125,10 +125,10 @@ else:
     logger.debug('Using WinForms / MSHTML')
     renderer = 'mshtml'
 
-if not _incognito:
+if not _private_mode:
     try:
         app_data = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
-        cache_dir = os.path.join(app_data, 'pywebview')
+        cache_dir = _storage_path or os.path.join(app_data, 'pywebview')
 
         if not os.path.exists(cache_dir):
             os.makedirs(cache_dir)

commit b14e0e65b34c5acc69aab1e95e0a721c647ec238

Catch KeyError on `backspaceKeyNavigationEnabled` Originally added in PR #294 (and this crash discussed in issue #210), but broken in b40511d. This change also switches from Foundation.YES/NO to True/False for consistency with usage elsewhere.

webview/platforms/cocoa.py
@@ -386,11 +386,14 @@ class BrowserView:
             self.datastore = WebKit.WKWebsiteDataStore.defaultDataStore()
             config.setWebsiteDataStore_(self.datastore)
 
-        config.preferences().setValue_forKey_(Foundation.NO, 'backspaceKeyNavigationEnabled')
+        try:
+            config.preferences().setValue_forKey_(False, 'backspaceKeyNavigationEnabled')
+        except KeyError:
+            pass  # backspaceKeyNavigationEnabled does not exist prior to macOS Mojave
         config.preferences().setValue_forKey_(True, 'allowFileAccessFromFileURLs')
 
         if _debug['mode']:
-            config.preferences().setValue_forKey_(Foundation.YES, 'developerExtrasEnabled')
+            config.preferences().setValue_forKey_(True, 'developerExtrasEnabled')
 
         self.js_bridge = BrowserView.JSBridge.alloc().initWithObject_(window)
         config.userContentController().addScriptMessageHandler_name_(self.js_bridge, 'jsBridge')