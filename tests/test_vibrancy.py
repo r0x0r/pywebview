7f64ecf2a0e7e061c39351eabba1591f2bcc3578
Jian-Hong Pan <chienhung.pan@gmail.com>
2023-11-26 23:08:02 +0800

    [tests] Fix test_vibrancy.py with pytest format
    Fix test_vibrancy.py with pytest format. And, skip it if it is not on
    macOS. The vibrancy is macOS only feature. [1]
    
    [1]: https://pywebview.flowrl.com/guide/api.html#webview-create-window

--- tests/test_vibrancy.py
+++ tests/test_vibrancy.py
@@ -1,15 +1,20 @@
+import sys
+import pytest
 import webview
+
+from .util import run_test
 
 
 def load_css(window):
     window.load_css('body { background: transparent !important; }')
 
 
-if __name__ == '__main__':
+@pytest.mark.skipif(sys.platform != 'darwin', reason='vibrancy is macOS only')
+def test_vibrancy():
     window = webview.create_window(
         'set vibrancy example',
         'https://pywebview.flowrl.com/hello',
         transparent=True,
         vibrancy=True,
     )
-    webview.start(load_css, window)
+    run_test(webview, window, start_args={'func': load_css, 'args': window})
