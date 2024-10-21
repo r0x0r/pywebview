346c6a3cbe7fa625784bec673cb1db90377a3c0c
r0x0r <roman@flowrl.com>
2024-10-21 14:22:22 +0300

    Fix pyinstaller compatibility

---
+++ webview/__pyinstaller/hook-webview.py
@@ -0,0 +1,8 @@
+from PyInstaller.utils.hooks import collect_data_files, collect_dynamic_libs
+from PyInstaller.compat import is_win
+
+if is_win:
+    datas = collect_data_files('webview', subdir='lib')
+    binaries = collect_dynamic_libs('webview')
+
+datas += collect_data_files('webview', subdir='js')
