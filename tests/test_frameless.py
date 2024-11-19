7dd8999e9ae32396c2e48cfb36fb8de894b7d07f
r0x0r <roman@flowrl.com>
2024-11-19 23:51:57 +0200

    Disable test

--- tests/test_frameless.py
+++ tests/test_frameless.py
@@ -1,8 +1,10 @@
+import os
 import webview
 import pytest
 from .util import run_test
 
 
+@pytest.mark.skipif(os.environ.get('PYWEBVIEW_GUI') == 'qt', reason='This test  crashes PyQt5')
 def test_frameless():
     window = webview.create_window('Frameless test', 'https://www.example.org', frameless=True)
     run_test(webview, window)
