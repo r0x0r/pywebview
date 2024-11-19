d491d027808005f0b92126ec2160788122d824f6
r0x0r <roman@flowrl.com>
2024-11-19 23:30:42 +0200

    Debug tests

--- tests/test_frameless.py
+++ tests/test_frameless.py
@@ -3,7 +3,6 @@ import pytest
 from .util import run_test
 
 
-@pytest.mark.skip
 def test_frameless():
     window = webview.create_window('Frameless test', 'https://www.example.org', frameless=True)
     run_test(webview, window)
