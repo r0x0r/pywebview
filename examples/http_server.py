5a2e591769423e239b010be377d6baa41ab0e3d7
r0x0r <roman@flowrl.com>
2025-01-07 23:48:09 +0200

    Update http server example

--- examples/http_server.py
+++ examples/http_server.py
@@ -2,7 +2,11 @@
 
 import webview
 
+
 if __name__ == '__main__':
-    webview.create_window('My first HTML5 application', 'assets/index.html')
     # HTTP server is started automatically for local relative paths
+    webview.create_window('My first HTML5 application', 'assets/index.html')
+
+    # Server certificate is self signed so we need to ignore SSL errors
+    webview.settings['IGNORE_SSL_ERRORS'] = True
     webview.start(ssl=True)
