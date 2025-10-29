1549440b4e44c987f704df974d5fcc96e31d17e4
r0x0r <roman@flowrl.com>
2025-10-29 23:17:12 +0200

    Support for state index notation

--- webview/state.py
+++ webview/state.py
@@ -44,17 +44,15 @@ class State(dict):
         t = Thread(target=notify_handlers)
         t.start()
 
-    def __setattr__(self, key, value, should_update_js=True):
+    def _set_state_value(self, key, value, should_update_js=True):
+        """Internal method to set state values, used by both __setattr__ and __setitem__"""
+
         def update_and_notify():
             self.__update_js(key, value)
             self.__notify_handlers(StateEventType.CHANGE, key, value)
 
-        if key.startswith('_State__'):
-            super().__setattr__(key, value)
-            return
-
-        if key not in self or self[key] != value:
-            self[key] = value
+        if key not in self or super().get(key) != value:
+            super().__setitem__(key, value)
 
             if not should_update_js:
                 self.__notify_handlers(StateEventType.CHANGE, key, value)
@@ -65,10 +63,31 @@ class State(dict):
             else:
                 self.__window.events.loaded += update_and_notify
 
+    def __setattr__(self, key, value, should_update_js=True):
+        if key.startswith('_State__'):
+            super().__setattr__(key, value)
+            return
+
+        self._set_state_value(key, value, should_update_js)
+
     def __getattr__(self, key):
         if key in self:
             return self[key]
         raise AttributeError(f"'{type(self).__key__}' object has no attribute '{key}'")
+
+    def _delete_state_value(self, key, should_update_js=True):
+        """Internal method to delete state values, used by both __delattr__ and __delitem__"""
+        if key in self:
+            old_value = self[key]
+            super().__delitem__(key)
+
+            if should_update_js:
+                special_key = '__pywebviewHaltUpdate__' + key
+                self.__window.run_js(f'delete window.pywebview.state.{special_key}')
+
+            self.__notify_handlers(StateEventType.DELETE, key, old_value)
+            return old_value
+        return None
 
     def __delattr__(self, key):
         if key.startswith('__pywebviewHaltUpdate__'):
@@ -77,18 +96,23 @@ class State(dict):
         else:
             halt_update = False
 
-        if key in self:
-            old_value = self[key]
-            del self[key]
+        old_value = self._delete_state_value(key, should_update_js=not halt_update)
+        if old_value is None:
+            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{key}'")
 
-            if not halt_update:
-                special_key = '__pywebviewHaltUpdate__' + key
-                self.__window.run_js(f'delete window.pywebview.state.{special_key}')
+    def __getitem__(self, key):
+        """Support dictionary-style access: state['key']"""
+        return super().__getitem__(key)
 
-            self.__notify_handlers(StateEventType.DELETE, key, old_value)
+    def __setitem__(self, key, value):
+        """Support dictionary-style assignment: state['key'] = value"""
+        self._set_state_value(key, value, should_update_js=True)
 
-        else:
-            raise AttributeError(f"'{type(self).__key__}' object has no attribute '{key}'")
+    def __delitem__(self, key):
+        """Support dictionary-style deletion: del state['key']"""
+        old_value = self._delete_state_value(key, should_update_js=True)
+        if old_value is None:
+            raise KeyError(key)
 
     def __add__(self, item: Callable[..., Any]) -> Self:
         self.__event_handlers.append(item)
