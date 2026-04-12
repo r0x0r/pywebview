976eb7d2cd2e8982df31b346234e0ba904a548ce
r0x0r <roman@flowrl.com>
2026-04-13 00:04:06 +0300

    Better screens example

--- examples/screens.py
+++ examples/screens.py
@@ -4,9 +4,16 @@ import webview
 
 if __name__ == '__main__':
     screens = webview.screens
-    print('Available screens are: ' + str(screens))
+    print('Available screens:')
 
     for i, screen in enumerate(screens):
+        print(f'\nScreen {i + 1}:')
+        print(f'  Position: ({screen.x}, {screen.y})')
+        print(f'  Size: {screen.width}x{screen.height}')
+        print(f'  Scale: {screen.scale}x')
+        print(f'  DPI: {screen.dpi}')
+        print(f'  Physical Size: {screen.physical_width}x{screen.physical_height}')
+
         webview.create_window('', html=f'placed on the monitor {i + 1}', screen=screen)
 
     webview.start()
